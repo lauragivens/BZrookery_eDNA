a5c3f8900b9d623debc89360f4e42e6c84e03792
Laura Givens <l2givens27@gmail.com>
2025-04-01 17:22:52 -0400

    redo multiqc report without the undetermined barcodes included

--- 01_fastqc.sh
+++ 01_fastqc.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 conda activate fastqc 
+conda install multiqc
 fastqc --version 
 #v0.12.1
 
 # https://www.bioinformatics.babraham.ac.uk/projects/fastqc/INSTALL.txt
 # 
+
+# First, we are going to look at the quality of our reads  
 RAWDIR=/Volumes/Fuji/Mangroves/2025_0319_Givens_Canty_Rookery_COI
 OUTDIR=$RAWDIR/FQCreports
 mkdir -p $OUTDIR
 
 find $RAWDIR/fastq -type f -name "*.fastq" -o -name "*.fastq.gz" | xargs fastqc --threads 4 --outdir "$OUTDIR" 
+#makes two files for each fastq.gz file; an html and a zip file
 
+#aggregate all the fastqc files into a summary file 
+cd $OUTDIR
+mv Undetermined_* ../UnknownBarcodes #remove undetermined reads from multiqc analysis
+
+multiqc . #run multiqc
+mv multiqc* ../ #move the multiqc files out of the FQCreports file 
 
 #1 Apr 2025
 
