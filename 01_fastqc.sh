628076054efa7ee6eb2b57461bade09bbb4622df
Laura Givens <l2givens27@gmail.com>
2025-05-28 13:06:12 -0500

    update fastqc script

--- 01_fastqc.sh
+++ 01_fastqc.sh
@@ -4,13 +4,13 @@ conda install multiqc
 fastqc --version 
 #v0.12.1
 
+# install instructions for fastqc here:  
 # https://www.bioinformatics.babraham.ac.uk/projects/fastqc/INSTALL.txt
-# 
 
 # First, we are going to look at the quality of our reads  
-RAWDIR=/Volumes/Fuji/Mangroves/2025_0319_Givens_Canty_Rookery_COI
-OUTDIR=$RAWDIR/FQCreports_raw
-mkdir -p $OUTDIR
+RAWDIR=/Volumes/Fuji/Mangroves/2025_0319_Givens_Canty_Rookery_COI #set the RAWDIR variable to the location of the sequencing files  
+OUTDIR=$RAWDIR/FQCreports_raw #name the folder where you want the fastqc reports to go  
+mkdir -p $OUTDIR #make the OUTDIR folder
 
 find $RAWDIR/fastq -type f -name "*.fastq" -o -name "*.fastq.gz" | xargs fastqc --threads 4 --outdir "$OUTDIR" 
 #makes two files for each fastq.gz file; an html and a zip file
