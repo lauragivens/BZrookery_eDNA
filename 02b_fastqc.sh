02515125847a0bf624f5aea3b1a9bbfe2827078c
Laura Givens <42383647+lauragivens@users.noreply.github.com>
2025-08-11 12:01:22 -0500

    Update 02b_fastqc.sh

--- 02b_fastqc.sh
+++ 02b_fastqc.sh
@@ -12,8 +12,7 @@ cd /Volumes/Fuji/Mangroves/2025_0319_Givens_Canty_Rookery_COI
  find $DIR -type f -name "*.fastq" -o -name "*.fastq.gz" | xargs fastqc --threads 4 outdir "$REPORT"
 
 #aggregate all the fastqc files into a summary file 
-multiqc $DIR/reports-cutadapt/ --filename "multiqc-cutadapt-report" #
-
+multiqc $REPORT/ --filename "multiqc-cutadapt-report" #
 multiqc $REPORT/ --filename "multiqc-cutadapt-reads-report" #check overall quality of reads
 multiqc $REPORT/*R1* --filename "$REPORT/multiqc-cutadapt-reads-report-R1" #check quality of forward reads
 multiqc $REPORT/*R2* --filename "$REPORT/multiqc-cutadapt-reads-report-R2" #check quality of reverse reads
